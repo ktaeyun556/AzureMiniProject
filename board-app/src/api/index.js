1adcc14cbe7bfe94bb28c5342d6ca183751c6118
ktaeyun556 <ktaeyun556@gmail.com>
2025-07-21 15:41:58 +0900

    fix api baseURL

--- board-app/src/api/index.js
+++ board-app/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'https://ty-was2.azurewebsites.net',
+	baseURL: 'https://ty-was-2.azurewebsites.net',
 })
 
 instance.defaults.headers.post['Content-Type'] =
