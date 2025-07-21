8148ba91ca4ab76ae32d62430d04c699b18eb6e0
ktaeyun556 <ktaeyun556@gmail.com>
2025-07-21 13:23:04 +0900

    fix CORS

--- board-api/src/main/java/me/jsk/app/board/controller/BoardController.java
+++ board-api/src/main/java/me/jsk/app/board/controller/BoardController.java
@@ -22,8 +22,8 @@ import me.jsk.app.board.domain.ReplyVO;
 import me.jsk.app.board.service.BoardService;
 
 //@RestController
-@CrossOrigin(origins = "https://yellow-hill-0e278b900.1.azurestaticapps.net")
-//@CrossOrigin
+//@CrossOrigin(origins = "https://yellow-hill-0e278b900.1.azurestaticapps.net")
+@CrossOrigin
 @Controller
 @EnableAutoConfiguration
 @RequestMapping("/board")
