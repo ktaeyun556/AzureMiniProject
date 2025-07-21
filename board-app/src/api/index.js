df430b65755cf70693b9b6eb203ce8e7a967f1af
ktaeyun556 <ktaeyun556@gmail.com>
2025-07-21 09:34:31 +0900

    fix WEB api baseURL

--- board-app/src/api/index.js
+++ board-app/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'https://tywebapp-fpebgmdjc5eedjbm.canadacentral-01.azurewebsites.net',
+        baseURL: 'https://tywebapp-fpebgmdjc5eedjbm.canadacentral-01.azurewebsites.net',
 })
 
 instance.defaults.headers.post['Content-Type'] =
