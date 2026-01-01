b1a239a94f1752a5ee1935c48d026b3b6c698952
김태윤(Taeyoon Kim) <tykim@cloocus.com>
2026-01-02 08:24:48 +0900

    fix axios baseURL

--- board-app/src/api/index.js
+++ board-app/src/api/index.js
@@ -2,9 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
 	baseURL:
-		// 'https://sub3-was-g3dzdvedfmfqhaf2.koreacentral-01.azurewebsites.net', // sub3-was
-		// 'https://sub32-was-hrg0f5b4hbarhpdh.koreacentral-01.azurewebsites.net', //  sub32-was
-		'http://4.217.133.0:8090',
+		'http://4.230.107.50',
 })
 
 instance.defaults.headers.post['Content-Type'] =
