3612fe324787800a078d7507d6da1dbe67bdaf48
김태윤(Taeyoon Kim) <tykim@cloocus.com>
2026-01-30 15:48:15 +0900

    fix prettier error

--- board-app/src/api/index.js
+++ board-app/src/api/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL:
-		'https://api.tyaz.cloud',
+	baseURL: 'https://api.tyaz.cloud',
 })
 
 instance.defaults.headers.post['Content-Type'] =
